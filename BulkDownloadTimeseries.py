f905b27e6880f87acd1b80a5a480f07bf7464f75
NiklasSchwind <50081653+NiklasSchwind@users.noreply.github.com>
2022-10-04 13:07:27 +0200

    Update BulkDownloadTimeseries.py

--- BulkDownloadTimeseries.py
+++ BulkDownloadTimeseries.py
@@ -4,6 +4,7 @@ from DownloadingFunctions import BulkDownloadTimeSeries
 '''
 TODO: Fill out the following parameters to download the desired data
 The internal variable names of the CIE-API can be found in the Metadata script or in the CIE metadata https://cie-api.climateanalytics.org/api/meta/
+For an example on how to fill out the lists see the example.py script
 @param: SAVEPATH Path of the diretory on your computer the data should be saved at
 @param: format Format of the downloaded data, can be csv, png and pdf
 @param: Countries List of the Alpha-3 Codes of desired Countries, Countries available in the CIE can be found in the Metadata
